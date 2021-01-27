a5177e38514d594de836ad939aa8285f13af7f77
Milosz Golebiowski <73059737+golab368@users.noreply.github.com>
2021-01-27 23:16:02 +0000

    poprawaka
    polaczenie funkcji z app routa

--- app.py
+++ app.py
@@ -39,33 +39,36 @@ def index():
     return render_template("homepage.html", all_posts=all_posts)
 
 
+def create_entry_oredit_entry(entry_id, entry, form):
+    errors = None
+    if request.method == 'POST':
+        if form.validate_on_submit() and entry_id:
+            form.populate_obj(entry)
+            db.session.commit()
+            return redirect("/homepage")
+        elif form.validate_on_submit():
+            db.session.add(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
+    return render_template('entry_form.html', form=form, errors=errors, entry_id=entry_id)
 
-@app.route("/new-post/", methods=["GET", "POST"])
+@app.route("/new-post/", methods=['GET', 'POST'])
 def create_entry():
-   form = EntryForm()
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           entry = Entry(
-               title=form.title.data,
-               body=form.body.data,
-               is_published=form.is_published.data
-           )
-           db.session.add(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
+    form = EntryForm()
+    entry_id = None
+    entry = Entry(
+        title=form.title.data,
+        body=form.body.data,
+        is_published=form.is_published.data
+        )
+    return create_or_edit_entry(entry_id, entry, form)
 
-@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+
+@app.route("/edit-post/<int:entry_id>", methods=['GET', 'POST'])
 def edit_entry(entry_id):
-   entry = Entry.query.filter_by(id=entry_id).first_or_404()
-   form = EntryForm(obj=entry)
-   errors = None
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           form.populate_obj(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
-   return render_template("entry_form.html", form=form, errors=errors)
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    form = EntryForm(obj=entry)
+    return create_or_edit_entry(entry_id, entry, form)
+    
+
