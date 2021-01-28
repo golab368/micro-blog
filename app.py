93294aa63e58385b17cfbe81d864d3ee60d77400
Milosz Golebiowski <73059737+golab368@users.noreply.github.com>
2021-01-28 00:26:35 +0000

    finish

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect,render_template,session, flash,  url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-from flask import render_template
+import functools
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
-
+import os
 
 app=Flask(__name__)
+
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "pass")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
@@ -29,6 +32,54 @@ class EntryForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     body = TextAreaField('Content', validators=[DataRequired()])
     is_published = BooleanField('Is Published?')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+
+    def validate_username(self, field):
+        if field.data != ADMIN_USERNAME:
+            raise ValidationError("Invalid username")
+        return field.data
+
+    def validate_password(self, field):
+        if field.data != ADMIN_PASSWORD:
+            raise ValidationError("Invalid password")
+        return field.data
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
+
 
 
 @app.route("/")
@@ -54,6 +105,7 @@ def create_entry_oredit_entry(entry_id, entry, form):
     return render_template('entry_form.html', form=form, errors=errors, entry_id=entry_id)
 
 @app.route("/new-post/", methods=['GET', 'POST'])
+@login_required
 def create_entry():
     form = EntryForm()
     entry_id = None
@@ -66,9 +118,22 @@ def create_entry():
 
 
 @app.route("/edit-post/<int:entry_id>", methods=['GET', 'POST'])
+@login_required
 def edit_entry(entry_id):
     entry = Entry.query.filter_by(id=entry_id).first_or_404()
     form = EntryForm(obj=entry)
     return create_entry_oredit_entry(entry_id, entry, form)
     
 
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
+
+@app.route("/delete/<int:draft_id>", methods=['POST'])
+def delete_entry(draft_id):
+    draft = Entry.query.filter_by(id=draft_id).first_or_404()
+    db.session.delete(draft)
+    db.session.commit()
+    return redirect("/")
